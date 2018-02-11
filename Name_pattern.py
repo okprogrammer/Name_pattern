dab0e30badf179f386e289816c466235374b6b63
okprogrammmer <omk7912@gmail.com>
2018-02-11 16:23:34 +0530

    Add this pattern

---
+++ Name_pattern.py
@@ -0,0 +1,78 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+ime = input("Enter your name: \n\n") 
+lngth = len(ime) 
+print("This is the pattern of your name")
+
+for x in range(0, lngth): 
+    c = ime[x] 
+    c = c.upper() 
+    if (c == "A"): 
+        print("  ******  \n  *    *  \n  ******  \n  *    *  \n  *    * \n\n") 
+    elif (c == "B"): 
+        print("  ******  \n  *    *  \n  *****   \n  *    *  \n  ******  \n\n") 
+    elif (c == "C"): 
+        print("  ******   \n  *  \n  *       \n  *       \n  ******  \n\n") 
+    elif (c == "D"): 
+        print("  *****   \n  *    *  \n  *    *  \n  *    *  \n  *****   \n\n") 
+    elif (c == "E"): 
+        print("  ******  \n  *       \n  *****   \n  *       \n  ******  \n\n") 
+    elif (c == "F"): 
+        print("  ******  \n  *       \n  *****   \n  *       \n  *       \n\n") 
+    elif (c == "G"): 
+        print("  *******  \n  *       \n  *   ***   \n  *     *  \n  *******  \n\n") 
+    elif (c == "H"): 
+        print("  *    *  \n  *    *  \n  ******  \n  *    *  \n  *    * \n\n") 
+    elif (c == "I"): 
+        print("    **    \n    **    \n    **    \n    **    \n          \n\n") 
+    elif (c == "J"): 
+        print("  ******  \n    **    \n    **    \n  * **  \n  ****    \n\n") 
+    elif (c == "K"): 
+        print("  *   *   \n  *  *    \n  **      \n  *  *    \n  *   *   \n\n") 
+    elif (c == "L"): 
+        print("  *      \n  *                  \n  *       \n  *       \n  ******  \n\n") 
+    elif (c == "M"): 
+        print("  *    *   \n  **  **  \n  * ** *  \n  *    *   \n  *    *  \n\n") 
+    elif (c == "N"): 
+        print("  *    *  \n  **   *  \n  * *  *  \n  *  * *  \n  *   **  \n\n") 
+    elif (c == "O"): 
+        print("  ******   \n  *    *  \n  *    *  \n  *    *  \n  ******  \n\n") 
+    elif (c == "P"): 
+        print("  ******  \n  *    *  \n  ******  \n  *       \n  *       \n\n") 
+    elif (c == "Q"): 
+        print("  ******  \n  *    *  \n  * *  *  \n  *  * *  \n  ******   \n       *            \n\n") 
+    elif (c == "R"): 
+        print("  ******  \n  *    * \n  * **   \n  *   *   \n  *    *  \n\n") 
+    elif (c == "S"): 
+        print("  ******    \n  *      \n  ******  \n       *  \n  ******   \n\n") 
+    elif (c == "T"): 
+        print("  ******  \n    **     \n    **    \n    **    \n    **     \n\n") 
+    elif (c == "U"): 
+        print("  *    *  \n  *    *   \n  *    *    \n  *    *    \n  ******   \n\n") 
+    elif (c == "V"): 
+        print("  *    *  \n  *    *  \n  *    *  \n   *  *   \n    **    \n\n") 
+    elif (c == "W"): 
+        print("  *    *  \n  *    *  \n  * ** *  \n  **  ** \n  *    *  \n\n") 
+    elif (c == "X"): 
+        print("  *    *  \n   *  *   \n    **    \n   *  *   \n  *    *  \n\n") 
+    elif (c == "Y"): 
+        print("  *    *  \n   *  *   \n    **    \n    **    \n    **    \n\n") 
+    elif (c == "Z"): 
+        print("   ******    \n       *  \n     *    \n   *     \n  ******""  \n\n") 
+    elif (c == " "): 
+        print("..........\n..........\n..........\n..........\n\n") 
+    elif (c == "."): 
+        print("----..----\n\n") 
+
+print("______________________________________") 
+print("If you like it!!! Please follow me and hit ^ Up Vote.") 
+print("______________________________________") 
+print("Thanks for all the likes people! I have made this in Delphi for you") 
+print("There is a repo in Bitbucket, so if you want you can compile it yourself") 
+print("Or you can download exe just for fun :)") 
+print("REPO: https://kordarei@bitbucket.org/kordarei/solo_magic.git") 
+
